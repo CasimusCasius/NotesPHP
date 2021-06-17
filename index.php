9c4d4b1adc59ce35538ccf6f2923f2387ff85c3c
CasimusCasius <grzegorz.kazimierczak.sg@gmail.com>
2021-06-18 00:11:44 +0200

    Add debug function debug($data)

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<?php echo "PHP: hello";
+<?php 
+declare (strict_types=1);
+
+$test =  ['test', 'test1', 'test2'];
+
+function debug($data): void
+{
+echo '<div style="display: inline-block; padding: 0 5px; border: 2px solid grey; background: lightgray">
+    <pre>';
+        print_r($data);
+echo "</pre></div>";
+}
+
+debug($test);
